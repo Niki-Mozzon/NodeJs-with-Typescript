fdfc8bbd6eadb8f60622171139066eba6ed6c025
mozzon <niki.mozzon@gmail.com>
2022-05-21 23:29:34 +0200

    refactoring product controller

--- E-Commerce/root/controllers/ProductsController.ts
+++ E-Commerce/root/controllers/ProductsController.ts
@@ -5,23 +5,14 @@ import fetch from 'node-fetch';
 
 export class ProductsController {
   getProducts = async (req: Request, res: Response, next: NextFunction) => {
-    const response = await this.getData('http://localhost:4000/products').then(
-      (products) => {
+    const response = await fetch('http://localhost:4000/products')
+      .then((json) => json.json())
+      .then((products) => {
         res.render('main', {
           model: new PageModel('Products', 'products').setModel(products),
         });
-      }
-    );
+      });
   };
-
-  async getData(url: string): Promise<any> {
-    return fetch(url).then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    });
-  }
 
   addProduct = (req: Request, res: Response) => {
     const product: Product = new Product(req.body.productName);
